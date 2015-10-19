65dd037ee6c37c35157ba9949e749fa09945108c
lementec <lementec@esrf.fr>
2015-10-19 08:45:08 +0200

    host, line based protocol

--- readwrite/src/host/main.py
+++ readwrite/src/host/main.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
 
+#
 # very crude client to communicate with device
-# use python, pyserial and ascii to work across oses
+# use python, pyserial for cross oses portability
+# use synchronous ascii line based protocol to work with common term tools
+# use per command line acknowledge for flow control
+#
 # usage: main.py
 #  <device_path> rmem <offset> <size>
 #  <device_path> rrom
 #  <device_path> wmem <offset> <data_or_file>
-
-
-# TODO: read write by line
-# TODO: verify after write
-# TODO: ack after each command
+#
 
 
 import serial
@@ -19,55 +19,12 @@ import sys
 import os
 
 
-def makeline(s):
+def append_eol(s):
     return s + '\n'
 
 
-def stripline(l):
-    return l.replace('\r', '').replace('\n', '')
-
-
-def read_ack(ser):
-    # TODO
-    return 0
-
-
-def multiline(s):
-    # split a string into multiple lines to be sent
-    line_max_len = 16
-    s_len = len(s)
-    l = []
-    for i in range(0, s_len, line_max_len):
-        line_len = line_max_len
-        if s_len - i < line_max_len: line_len = s_len - i
-        l.append(s[i : i + line_len])
-    return l
-
-
-def do_read_mem(ser, av):
-    if len(av) != 2: return -1
-    off = av[0]
-    size = av[1]
-    s = makeline('rmem ' + off + ' ' + size)
-    ser.write(s)
-    return 0
-
-
-def do_read_rom(ser, av):
-    if len(av) != 0: return -1
-    s = makeline('rrom')
-    ser.write(s)
-    return 0
-
-
-def get_data_or_file(s):
-    # if s is a path, get the contents
-    # otherwise, s is hexadecimal data
-    if os.path.isfile(s) == False: return s
-    f = open(s, 'r')
-    s = f.read()
-    f.close()
-    return stripline(s)
+def strip_eol(l):
+    return l.rstrip('\n').l('\r')
 
 
 def is_hex(x):
@@ -76,7 +33,7 @@ def is_hex(x):
     return False
 
 
-def check_data(s):
+def check_data_line(s):
     n = len(s)
     if (n % 2) != 0: return -1
     for i in range(0, n):
@@ -84,17 +41,106 @@ def check_data(s):
     return 0
 
 
+max_line_len = 32
+def single_to_multi_line(s):
+    # split a string into multiple lines to be sent
+    s_len = len(s)
+    l = []
+    for i in range(0, s_len, max_line_len):
+        line_len = max_line_len
+        if s_len - i < max_line_len: line_len = s_len - i
+        l.append(s[i : i + line_len])
+    return l
+
+
+def size_to_line_count(s):
+    s_len = len(s)
+    n = s_len / max_line_len
+    if s_len % max_line_len: n = n + 1
+    return n
+
+
+def read_data_file(s):
+    # read a data file contents
+    # assume ascii file containing hex chars
+    # merge lines, if any
+    f = open(s, 'r')
+    x = f.read()
+    f.close()
+    lines = x.splitlines()
+    return lines.join()
+
+
+def data_or_file_to_multi_line(s):
+    # if s is a path, get the contents in a single line
+    # otherwise, s is hexadecimal data
+    if os.path.isfile(s) == True: s = read_data_file(s)
+    if check_data_line(s): return None
+    return single_to_multi_line(s)
+
+
+def recv_line(ser):
+    # receive a single line
+    # eol is stripped
+    return strip_eol(ser.readline())
+
+
+def recv_multi_line(ser, nline):
+    # receive all lines until ack
+    lines = []
+    while True:
+        l = recv_line(ser)
+        if l == None: break
+        lines.append(l)
+    return lines
+
+
+def send_line_recv_ack(ser, l):
+    ser.write(ser, append_eol(l))
+    return recv_line(ser)
+
+
+def send_cmd_and_recv_multi_line(ser, cmd_lines, nline):
+    for l in cmd_lines:
+        ack = send_line_recv_ack(ser, l)
+        if ack != 'ok': return None
+    repl_lines = []
+    for i in range(0, nline):
+        repl_lines.append(recv_line(ser))
+    return repl_lines
+
+
+def send_cmd(ser, lines):
+    send_cmd_and_recv_multi_line(ser, lines, 0)
+    return 0
+
+
+def do_read_mem(ser, av):
+    if len(av) != 2: return -1
+    off = av[0]
+    size = av[1]
+    cmd_line = [ 'rmem ' + off + ' ' + size ]
+    n = size_to_line_count(size)
+    repl_lines = send_cmd_and_recv_multi_line(ser, cmd_line, n)
+    for l in repl_lines: print(l)
+    return 0
+
+
+def do_read_rom(ser, av):
+    if len(av) != 0: return -1
+    cmd_line = [ 'rrom' ]
+    repl_lines = send_cmd_and_recv_multi_line(ser, cmd_line, 1)
+    for l in repl_lines: print(l)
+    return 0
+
+
 def do_write_mem(ser, av):
     if len(av) != 2: return -1
     off = int(av[0])
-    data = get_data_or_file(av[1])
-    if check_data(data): return -1
-    n = len(data)
-    for i in range(0, n / 2):
-        j = i * 2
-        x = data[j : j + 2]
-        s = makeline('wmem ' + str(off + i) + ' ' + x)
-        ser.write(s)
+    lines = data_or_file_to_multi_line(av[1])
+    if lines == None: return -1
+    lines.prepend('wmem ' + str(off))
+    send_cmd(ser, lines)
     return 0
 
 
